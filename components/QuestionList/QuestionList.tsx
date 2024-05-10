c9178d6e6d65e53f7d0396672b8cfb1f90bf4f1c
Jay Prakash Kalia <jaykalia047@gmail.com>
2024-05-11 03:07:30 +0530

    minor UI improvements

--- components/QuestionList/QuestionList.tsx
+++ components/QuestionList/QuestionList.tsx
@@ -1,38 +1,60 @@
-'use client';
+"use client";
 
-import { MOCK_QUESTIONS } from '@/mock-data';
-import { useRouter } from 'next/navigation';
-import { FC } from 'react';
-import { ChevronRight } from '../icons';
+import {
+  LOGGED_IN_USER_COMPLETED_QUESTIONS_ID,
+  MOCK_QUESTIONS,
+} from "@/mock-data";
+import { useRouter } from "next/navigation";
+import { FC, useState } from "react";
+import { CheckedCircle, ChevronRight } from "../icons";
+import { motion } from "framer-motion";
+import { Circle } from "lucide-react";
 
 type QuestionListItemProps = {
+  id: number;
   name: string;
   description: string;
   difficulty: string;
 };
 
 const QuestionListItem: FC<QuestionListItemProps> = ({
+  id,
   name,
   description,
   difficulty,
 }) => {
   const router = useRouter();
 
+  const [isHover, setIsHover] = useState(false);
+
+  console.log({ isHover });
+
   return (
     <div
-      className="p-4 flex justify-between items-center hover:bg-black/20 group cursor-pointer"
+      className="p-4 flex justify-between items-center hover:bg-black/20 cursor-pointer"
       onClick={() => {
-        router.push('/question');
+        router.push("/question");
       }}
+      onMouseEnter={() => setIsHover(true)}
+      onMouseLeave={() => setIsHover(false)}
     >
-      <div className="flex flex-col gap-0.5">
-        <p className="text-base font-medium">{name}</p>
-        <p className="text-sm text-gray-600 ">{description}</p>
-        <p className="text-sm text-green-600">{difficulty}</p>
+      <div className="flex items-center gap-6">
+        {LOGGED_IN_USER_COMPLETED_QUESTIONS_ID.includes(id) ? (
+          <CheckedCircle className="text-gray-600 h-6 w-6" fill="green" />
+        ) : (
+          <Circle className="text-gray-600 h-6 w-6" />
+        )}
+        <div className="flex flex-col gap-0.5">
+          <p className="text-base font-medium">{name}</p>
+          <p className="text-sm text-gray-600 ">{description}</p>
+          <p className="text-sm text-green-600">{difficulty}</p>
+        </div>
       </div>
-      <div className="group-hover:scale-125">
-        <ChevronRight />
-      </div>
+      <motion.div>
+        <motion.div animate={{ scale: isHover ? 1.7 : 1 }}>
+          <ChevronRight />
+        </motion.div>
+      </motion.div>
     </div>
   );
 };
@@ -46,6 +68,7 @@ const QuestionList: FC<QuestionListProps> = ({}) => {
         {MOCK_QUESTIONS.map(({ id, name, description, difficulty }) => (
           <QuestionListItem
             key={id}
+            id={id}
             name={name}
             description={description}
             difficulty={difficulty}
